5155e2e42441263762850f62de9173e5ded1e5c1
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-12-12 23:01:23 +0300

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header number={number < 5 ? 0 : number} />
+      <hr />
+
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
     </div>
   );
 }
 
 export default App;
+
+// "number" her değiştiğinde "State" değiştiği için <div> içerisindeki elemanlar render edilir. Dolayısıyla <Header> da gereksiz yere render edilir. Bunu engellemek için React.memo kullanılır.
